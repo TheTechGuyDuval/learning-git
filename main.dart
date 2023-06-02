43f6fddfc75c5abf9eea53767acc6554da3637ba
Choupo Duval <82890629+TheTechGuyDuval@users.noreply.github.com>
2023-06-02 23:12:48 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,26 +1,4 @@
 this is main
   
-  <!DOCTYPE html>
-<html>
 
-<head>
-	<title>Page Title</title>
-</head>
-
-<body>
-
-
-	
-	
-
-  <h1>This is a Heading</h1>
-  <p>This is a paragraph</p>
-  <p>This is another paragraph</p>	
-
-
-</body>
-
-
-
-</html>
 
